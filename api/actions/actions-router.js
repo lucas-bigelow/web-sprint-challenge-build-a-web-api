3566a7fb922c1a401970d33a78d10556d92338ff
Lucas Bigelow <lukejbigelow@gmail.com>
2022-06-10 15:48:24 -0500

    finalizing server set up and config

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,11 @@
 // Write your "actions" router here!
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  console.log('how is life going???');
+  res.end();
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const server = express();
 
+const actionsRouter = require('./actions/actions-router');
+
+server.use(actionsRouter);
+
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
